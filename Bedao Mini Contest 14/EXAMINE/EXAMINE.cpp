67aa8757f2db35f6ba1a169e977dd592e361f8a6
QioCas <106325835+QioCas@users.noreply.github.com>
2022-11-07 08:15:18 +0700

    Update EXAMINE.cpp

--- Bedao Mini Contest 14/EXAMINE/EXAMINE.cpp
+++ Bedao Mini Contest 14/EXAMINE/EXAMINE.cpp
@@ -5,20 +5,21 @@ using ll = long long;
 const ll inf = 2e18, mod = 1e9 + 7, LOG = 32, maxN = 200005;
 
 ll n;
-ll mul(ll a, ll b) {
+ll mul(ll a, ll b) { 
     ll r = 0; 
     while (b > 0) {
         if (b % 2 != 0) r = (r + a) % mod;
         a = a * 2 % mod;
         b = b / 2;
-        }
+    }
     return r % mod;
 }
 
 void Solve() {
     ll ans = 0;
+    cin >> n;
     for(ll i = 1; i * i <= n; ++i) {
-        if(n % i == 0 and __gcd(i, n / i) == 1) ans = (ans + mul(n, i) + mul(n, n / i) ) % mod;
+        if(n % i == 0 and __gcd(i, n / i) == 1) ans = (ans + mul(n, i) + mul(n, n / i)) % mod;
     }
     cout << ans << '\n';
 }
@@ -26,8 +27,5 @@ void Solve() {
 signed main() {
     cin.tie(NULL)->sync_with_stdio(false);
     int Q; cin >> Q;
-    while (Q--) {
-        cin >> n;
-        Solve();
-    }
+    while (Q--) Solve();
 }
