9c67e9c3e1dbbd80f86956a0c563ee7855a444c2
QioCas <106325835+QioCas@users.noreply.github.com>
2022-12-05 20:43:58 +0700

    Update main.cpp

--- Curent-Code/main.cpp
+++ Curent-Code/main.cpp
@@ -1,24 +1,12 @@
-#pragma GCC optimize("unroll-loops,02,no-stack-protector")
-#include "bits/stdc++.h" // QioCas
-#define int long long
+#include <bits/stdc++.h>
 using namespace std;
-int n , d;
-int a[2000006];
-int Sum(int x) {
-    return x * (x + 1) / 2LL;
-}
-signed main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    cin >> n >> d; 
-    int x; vector <int> Map (2000006 , 0);
-    for(int i = 1; i <= n; ++i ) cin >> x , Map[x]++;
-    for(int idx = 0, i = 1; i <= 1000000; ++i) 
-        while(Map[i]--) a[++idx] = i;
-    int ans = 0 , idx = 0;
-    for(int i = 3; i <= n; ++i) {
-        while(a[i] - a[idx+1] > d) idx++;
-        ans += max(0LL , Sum(i - idx - 2));
-    }
-
-    cout << ans;
+int n,a,b,res,cnt;
+int main(){
+	cin >> n;
+	while(n--){
+		cin >> a  >> b;
+		cnt=cnt-a+b;
+		res=max(res,cnt);
+	}
+	cout << res;
 }
