eb8aaad7d2f9f573bc34b28d81e6acceb8b64343
QioCas <106325835+QioCas@users.noreply.github.com>
2022-12-09 09:44:29 +0700

    Update main.cpp

--- Curent-Code/main.cpp
+++ Curent-Code/main.cpp
@@ -1,12 +1,29 @@
-#include <bits/stdc++.h>
+#define console(...) void() // 2022/12/09-09:16:17
+#include "bits/stdc++.h" // QioCas
 using namespace std;
-int n,a,b,res,cnt;
-int main(){
-	cin >> n;
-	while(n--){
-		cin >> a  >> b;
-		cnt=cnt-a+b;
-		res=max(res,cnt);
-	}
-	cout << res;
+using ll = long long;
+const ll inf = ll(2e18), mod = 1000000009, LOG = ' ', maxN = 200005;
+
+int n, Q;
+ll a[maxN];
+void Enter() {
+    cin >> n >> Q;
+    for(int i = 1; i <= n; ++i) cin >> a[i];
+}
+ll find(ll q) {
+    ll id = upper_bound(a + 1, a + n + 1, q) - a - 1;
+    return q + id;
+}
+void Solve() {
+    sort(a + 1, a + n + 1);
+    for(int  i = 2; i <= n; ++i) a[i] -= i - 1;
+    for(int i = 1; i <= Q; ++i) {
+        ll c; cin >> c;
+        cout << find(c) << '\n';
+    }
+}
+signed main() { 
+    cin.tie(0)->sync_with_stdio(0);
+    Enter();
+    Solve();
 }
